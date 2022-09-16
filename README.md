12323974072b8af72d0448947c31831421c6b4ea
Julio Negri <julioyuki@outlook.com>
2022-09-16 08:35:05 -0300

    feat: starting to implement the scanner

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # RLOX
 
 The Rlox are my rust implementation of Lox language on "Crafting Interpreters" book
+
+### TODO
+
+- Separate my current code on different modules to be easier to work with
+- Advance more on the book reading
+- Study more about error handling to guarantee that the user knows what is wrong
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,76 @@
 use std::env;
 use std::io::{Read, stdin};
 use std::fs::File;
+use std::fmt::Display;
 
 // TODO study more custom errors to handle better
+
+enum TokenType {
+    // ? single-character tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // ? one or two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // ? literals
+    IDENTIFIER, STRING, NUMBER,
+
+    //? keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF,
+    NIL, OR, PRINT, RETURN, SUPER, THIS,
+    TRUE, VAR, WHILE,
+    EOF
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+       write!(f, "Token Type zika") // TODO improve this Display fmt
+    }
+}
+
+struct Scanner {
+    source: String,
+    tokens: Vec<Token>
+}
+
+impl Scanner {
+    fn scanTokens() { }
+}
+
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: String,
+    line: u16
+}
+
+impl Token {
+    fn new() -> Token {
+        Token {
+            token_type: TokenType::AND,
+            lexeme: String::new(),
+            literal: String::new(),
+            line: 0
+        }
+    }
+}
+
+impl ToString for Token {
+    fn to_string(&self) -> String {
+        format!("{} {} {}", self.token_type, self.lexeme, self.literal)
+    }
+}
 
 enum LoxErrors {
     ParserError {
